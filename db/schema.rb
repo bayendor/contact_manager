8926674ad0b5ec3021dd48af1b84c3e683dc9fe0
David Bayendor <dbayendor@gmail.com>
2014-11-14 10:29:49 -0700

    Change Phone Numbers to Contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113170613) do
+ActiveRecord::Schema.define(version: 20141114160453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(version: 20141113170613) do
 
   create_table "phone_numbers", force: true do |t|
     t.string   "number"
-    t.integer  "person_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "contact_id"
+    t.string   "contact_type"
   end
 
 end
